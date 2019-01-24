f0c100a230782582ef811dd9da33d1a87ee6d392
izhan25 <izhan.yameen25@gmail.com>
2019-01-25 01:28:03 +0500

    Updates

---
+++ precache-manifest.aee625b240e3aa8fd33a95f31c6d02c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "03daa8a936e4ddb431a7",
+    "url": "/contactmanager/static/js/main.03daa8a9.chunk.js"
+  },
+  {
+    "revision": "8d5f7a03c49eb877e342",
+    "url": "/contactmanager/static/js/1.8d5f7a03.chunk.js"
+  },
+  {
+    "revision": "03daa8a936e4ddb431a7",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "8d5f7a03c49eb877e342",
+    "url": "/contactmanager/static/css/1.6f84c195.chunk.css"
+  },
+  {
+    "revision": "a3e253382a548e0fb10a49a055597d24",
+    "url": "/contactmanager/index.html"
+  }
+];
